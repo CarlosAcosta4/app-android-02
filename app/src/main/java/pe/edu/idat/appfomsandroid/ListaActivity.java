9497beddcef3558aa9fec9ea3416b0f160d72f5c
CarlosAcosta4 <104353142+CarlosAcosta4@users.noreply.github.com>
2023-10-29 23:44:50 -0500

    first commit

---
+++ app/src/main/java/pe/edu/idat/appfomsandroid/ListaActivity.java
@@ -0,0 +1,37 @@
+package pe.edu.idat.appfomsandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
+import pe.edu.idat.appfomsandroid.databinding.ActivityListaBinding;
+
+public class ListaActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ActivityListaBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityListaBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ArrayList<String> listaPersonas = (ArrayList<String>)
+                getIntent().getSerializableExtra("listapersonas");
+        ArrayAdapter arrayAdapter = new ArrayAdapter(
+                this, android.R.layout.simple_list_item_1,
+                        listaPersonas
+        );
+        binding.lvpersonas.setAdapter(arrayAdapter);
+        binding.btnregreasarregistro.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        startActivity(new Intent(getApplicationContext(),RegistroActivity.class));
+    }
+}
